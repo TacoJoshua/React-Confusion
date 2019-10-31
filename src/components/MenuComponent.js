a2d99da505a7a6dd05f411c2d95351617488ccf0
Joshua Jackson <j.shuajackson@gmail.com>
2019-10-30 19:58:58 -0700

    Header and Footer

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,40 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class Menu extends Component {
-
-
-    renderDish(dish) {
-        if (dish != null) {
-            return(
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
-        }
-        else {
-            return(
-                <div></div>
-            );
-        }
+    function RenderMenuItem({dish, onClick}) {
+        return(
+            <Card onClick={() => onClick(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        );
     }
+    
+    const Menu = (props) => {
 
-    render() {
-        const menu = this.props.dishes.map((dish) => {
+        const menu = props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card onClick={() => this.props.onClick(dish.id)}>
-                      <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
+                <RenderMenuItem dish={dish} onClick={props.onClick} />
               </div>
             );
         });
@@ -47,6 +30,6 @@ class Menu extends Component {
           </div>
         );
     }
-}
+        
 
 export default Menu;
